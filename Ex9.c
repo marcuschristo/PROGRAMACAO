bb2dae7445a5483c83883925561629fa2d0085a8
marcuschristo <89815788+marcuschristo@users.noreply.github.com>
2021-11-22 20:31:42 -0300

    Add files via upload

---
+++ Ex9.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+//Função principal do programa
+int main(int argc, char *argv[]){
+
+//Declaração de variáveis
+float a, b, comp;
+
+//Solicita entrada de dados para o usuário e grava o valor digitado em uma variável "a"
+printf("Digite o valor de a:");
+scanf("%f", &a);
+
+//Solicita entrada de dados para o usuário e grava o valor digitado em uma variável "b"
+printf("Digite valor de b:");
+scanf("%f", &b);
+
+//Faz a comparação dos valores obtidos acima
+comp == (a,b);
+
+//Decisão baseada na comparação
+if (a>=b);
+{printf(""a" é maior que "b"");
+}   else
+{printf(""b" é maior que "a"");
+} 
+return 0;
+}
